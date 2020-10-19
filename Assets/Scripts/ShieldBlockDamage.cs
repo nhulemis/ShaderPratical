015748cd619bb97a44b2f41d67edcce504436599
nhulemis <nhulemis@gmail.com>
2020-10-19 15:17:52 +0700

    Add FX shield

---
+++ Assets/Scripts/ShieldBlockDamage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldBlockDamage : MonoBehaviour
+{
+    public GameObject BlockDamageDone;
+
+    private Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision co)
+    {
+        Debug.Log("blockDameDone");
+        var blockDameDone = Instantiate(BlockDamageDone, transform);
+        var pSR = blockDameDone.GetComponent<ParticleSystemRenderer>();
+        mat = pSR.material;
+        mat.SetVector("_blockDameCenter", co.contacts[0].point);
+
+        Destroy(blockDameDone, 2);
+
+    }
+}
